d9d3faa63337538e7658810cb9f8eaddbeaa3cb0
Heros Perez <119890520+hersdev199@users.noreply.github.com>
2024-08-08 16:30:21 +0000

    Image - background

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main
@@ -30,7 +32,7 @@ export default function Home() {
           <input
             type="submit"
             id="send"
-            className="w-[85%] m-2 border rounded-[16px] h-[40px] bg-[#18c4a4] text-white"
+            className="w-[85%] m-2 border rounded-[16px] h-[40px] bg-[#18c4a4] text-white p-2"
             value="Iniciar Sesión"
           />
         </div>
@@ -41,6 +43,15 @@ export default function Home() {
             Haz click aquí
           </a>
         </p>
+      </div>
+      <div className="absolute inset-0 z-0">
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-20"
+        />
       </div>
 
       <footer id="footer" className="sticky bottom-0 mb-5 w-full text-center">
